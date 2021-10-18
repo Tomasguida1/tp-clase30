5be0d1a812e4d0870d16ffa0aa180beb77dc7342
tomasguida <tomasguida2000@hotmail.com>
2021-10-18 14:35:25 -0300

    se prueba si intento construir un empleado con el nombre vacio

--- tests/empleadotest.php
+++ tests/empleadotest.php
@@ -1,9 +1,14 @@
 <?php
 abstract class empleadotest extends \PHPUnit\Framework\TestCase
 {
-   public function_construct($nombre="luis", $apellido ="Gonzales",$dni = 2245598, $salario = 40000)
+   public function construct($nombre="luis", $apellido ="Gonzales",$dni = 2245598, $salario = 40000)
     {
        $re = new \App\EmpleadoEventual($nombre, $apellido, $dni, $salario);
        return $re;
     }
+   public function nombrevacio() 
+   {
+    $this->expectException(\Exception::class);
+    $r = $this->construct("");
+ }
 }
